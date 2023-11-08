a2e00f90c599ada478bccb0f04bab78695e1df5a
marwasabon <marwasabon@gmail.com>
2023-11-08 18:33:29 +0000

    fixed style & code

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -5,8 +5,9 @@ import unittest
 import uuid
 from datetime import datetime
 
+
 class TestBaseModel(unittest.TestCase):
-    
+
     def test_init(self):
         base_model = BaseModel()
         self.assertIsInstance(base_model.id, str)
@@ -15,15 +16,16 @@ class TestBaseModel(unittest.TestCase):
 
     def test_str(self):
         base_model = BaseModel()
-        self.assertEqual(str(base_model), "[BaseModel] ({}) {}".format(base_model.id, base_model.__dict__))
-
+        expected_str = "[BaseModel] ({}) {}".
+        format(base_model.id, base_model.__dict__)
+        self.assertEqual(str(base_model), expected_str)
 
     def test_save(self):
         base_model = BaseModel()
         initial_updated_at = base_model.updated_at
         base_model.save()
         self.assertNotEqual(initial_updated_at, base_model.updated_at)
-    
+
     def test_data(self):
         my_model = BaseModel()
         my_model.name = "My First Model"
@@ -34,18 +36,21 @@ class TestBaseModel(unittest.TestCase):
         my_model_json = my_model.to_dict()
         print(my_model_json)
         print("JSON of my_model:")
-        for key in my_model_json.keys():
-            print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-        
+        print("\t{}: ({}) - {}".format(
+                key, type(my_model_json[key]), my_model_json[key]
+                ))
+
     def test_to_dict(self):
         base_model = BaseModel()
         obj_dict = base_model.to_dict()
         self.assertIsInstance(obj_dict, dict)
         self.assertEqual(obj_dict['__class__'], 'BaseModel')
-        self.assertEqual(obj_dict['created_at'], base_model.created_at.isoformat())
-        self.assertEqual(obj_dict['updated_at'], base_model.updated_at.isoformat())
-
-
+        self.assertEqual(
+                    obj_dict['created_at'], base_model.created_at.isoformat()
+                    )
+        self.assertEqual(
+                    obj_dict['updated_at'], base_model.updated_at.isoformat()
+                    )
 
 
 if __name__ == "__main__":
