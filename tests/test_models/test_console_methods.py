6db1db6d12dcb4ca89edca468a256563ecbcba77
Marwa Jaberallah Sabon <marwasabon@gmail.com>
2023-11-13 18:50:38 +0200

    Update test_console_methods.py

--- tests/test_models/test_console_methods.py
+++ tests/test_models/test_console_methods.py
@@ -1,85 +1,62 @@
-#!/usr/bin/python3
-
-from models.base_model import BaseModel
-from console import HBNBCommand
 import unittest
-from unittest.mock import patch
 from console import HBNBCommand
-from unittest import TestCase
+from models import storage
 from io import StringIO
+from unittest.mock import patch
 
 
-class TestConsole(TestCase):
+class TestConsole(unittest.TestCase):
+    '''Test for console module'''
+
     def setUp(self):
+        '''Set up the test'''
         self.console = HBNBCommand()
 
-    def test_create_missing_class_name(self):
-        result = self.console.onecmd('create')
-        self.assertEqual(result, '** class name missing **')
+    def tearDown(self):
+        '''Clean up the test'''
+        del self.console
 
-    def test_create_class_doesnt_exist(self):
+    def test_create(self):
+        '''Test the create method'''
         with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd('create MyModel')
-            self.assertEqual(f.getvalue().strip(), '** class doesn\'t exist **')
+            self.console.onecmd("create BaseModel")
+            self.assertTrue(str(storage.all().values()) != "")
 
-    def test_show_missing_class_name(self):
-        result = self.console.onecmd('show')
-        self.assertEqual(result, '** class name missing **')
-
-    def test_show_class_doesnt_exist(self):
+    def test_show(self):
+        '''Test the show method'''
         with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd('show MyModel')
-            self.assertEqual(f.getvalue().strip(), '** class doesn\'t exist **')
+            self.console.onecmd("show BaseModel 12345")
+            self.assertTrue(f.getvalue().strip() == "** no instance found **")
 
-    def test_show_missing_instance_id(self):
-        result = self.console.onecmd('show BaseModel')
-        self.assertEqual(result, '** instance id missing **')
-
-    def test_show_no_instance_found(self):
+    def test_all(self):
+        '''Test the all method'''
         with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd('show BaseModel 121212')
-            self.assertEqual(f.getvalue().strip(), '** no instance found **')
+            self.console.onecmd("all")
+            self.assertFalse(f.read() == "[]\n")
 
-    def test_destroy_missing_class_name(self):
-        result = self.console.onecmd('destroy')
-        self.assertEqual(result, '** class name missing **')
-
-    def test_destroy_class_doesnt_exist(self):
+    def test_update(self):
+        '''Test the update method'''
         with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd('destroy MyModel')
-            self.assertEqual(f.getvalue().strip(), '** class doesn\'t exist **')
+            self.console.onecmd("update BaseModel 12345")
+            self.assertTrue(f.getvalue().strip() == "** no instance found **")
 
-    def test_destroy_missing_instance_id(self):
-        result = self.console.onecmd('destroy BaseModel')
-        self.assertEqual(result, '** instance id missing **')
-
-    def test_destroy_no_instance_found(self):
+    def test_destroy(self):
+        '''Test the destroy method'''
         with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd('destroy BaseModel 121212')
-            self.assertEqual(f.getvalue().strip(), '** no instance found **')
+            self.console.onecmd("destroy BaseModel 12345")
+            self.assertTrue(f.getvalue().strip() == "** no instance found **")
 
-    def test_update_missing_class_name(self):
-        result = self.console.onecmd('update')
-        self.assertEqual(result, '** class name missing **')
-
-    def test_update_class_doesnt_exist(self):
+    def test_quit(self):
+        '''Test the quit method'''
         with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd('update MyModel')
-            self.assertEqual(f.getvalue().strip(), '** class doesn\'t exist **')
+            self.assertTrue(self.console.onecmd("quit") is True)
 
-    def test_update_missing_instance_id(self):
-        result = self.console.onecmd('update BaseModel')
-        self.assertEqual(result, '** instance id missing **')
+    def test_EOF(self):
+        '''Test the EOF method'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertTrue(self.console.onecmd("EOF") is True)
 
-    def test_update_missing_attribute_name(self):
-        result = self.console.onecmd('update BaseModel existing-id')
-        self.assertEqual(result, '** attribute name missing **')
-
-    def test_update_missing_value_for_attribute(self):
-        result = self.console.onecmd('update BaseModel existing-id first_name')
-        self.assertEqual(result, '** value missing **')
-
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_empty_line(self):
+        '''Test the empty line method'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertTrue(self.console.onecmd(" ") is None)
