4342ad6910ff037fcca1b64c2789bc399f0b045b
Marwa Jaberallah Sabon <marwasabon@gmail.com>
2023-11-15 01:37:54 +0200

    Update console.py

--- console.py
+++ console.py
@@ -191,6 +191,23 @@ class HBNBCommand(cmd.Cmd):
             print("** attribute name missing **")
         except ValueError:
             print("** value missing **")
+    
+    def count(self, line):
+        """count the number of instances of a class
+        """
+        counter = 0
+        try:
+            my_list = split(line, " ")
+            if my_list[0] not in self.class_dict:
+                raise NameError()
+            objects = storage.all()
+            for key in objects:
+                name = key.split('.')
+                if name[0] == my_list[0]:
+                    counter += 1
+            print(counter)
+        except NameError:
+            print("** class doesn't exist **")
 
 
 if __name__ == "__main__":
